1042c2bf2be7b628470e5f3f582814b6cc161e84
Abhiroop25902 <abhiroop.m25902@gmail.com>
2021-11-11 19:28:06 +0530

    [ADD] added q5 -> assignment done

---
+++ Sheet_5/q5.sql
@@ -0,0 +1,35 @@
+create table emp(
+    emp_id numeric(10) primary key,
+    salary numeric(10) default 0
+);
+
+insert into emp values (510719007, 100000);
+insert into emp values (510719008, 110000);
+insert into emp values (510719009, 120000);
+insert into emp values (510719010, 900000);
+
+select * from emp order by emp;
+
+create or replace function raiseWage(in id numeric(10), in amount numeric(10))
+    returns void 
+    language plpgsql as $$
+        begin
+            if(id not in (select E.emp_id from emp E)) then
+                raise exception 'nonexistent ID --> %', emp_id
+                    using hint= 'Please check your emp_id';
+            elsif (amount is null) then
+              raise exception 'amount can''t be null'
+                    using hint= 'Please check your amount';
+            end if;
+
+            update emp
+               set salary=salary + amount
+             where emp_id = id;
+        end;
+    $$;
+
+select raiseWage(510719010,1000);
+
+select * from emp order by emp;
+drop function raiseWage;
+drop table emp;
