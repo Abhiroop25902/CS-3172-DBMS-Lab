50d1b6aff5a47602dbe0f960510ebd94c6d16025
Abhiroop25902 <abhiroop.m25902@gmail.com>
2021-11-18 01:53:23 +0530

    [FIX] fixed q4 according to question

--- Sheet_5/q4.sql
+++ Sheet_5/q4.sql
@@ -1,51 +1,43 @@
-create table item_tr(
+create table item_mstr(
     item_id numeric(10) primary key,
     quantity numeric(5)
 );
 
-create table item_mstr(
-    item_id numeric(10) primary key references item_tr(item_id) on delete cascade,
+create table item_tr(
+    item_id numeric(10) primary key references item_mstr(item_id) on delete cascade,
     quantity numeric(5)
 );
-select * from item_tr;
-select * from item_mstr;
+
+insert into item_mstr values (10001, 10);
+insert into item_mstr values (10002, 9);
+insert into item_mstr values (10003, 8);
+
 
 create or replace function processEdit()
     returns trigger
     language plpgsql as $$
         begin
-            if(tg_op = 'INSERT') then --capitalization necessary here
-                insert into item_mstr select new.*;
-            elsif(tg_op= 'UPDATE') then --capitalization necessary here
-                update item_mstr set quantity = new.quantity where item_id = new.item_id;
-            end if;
-
+            update item_mstr set quantity = quantity - new.quantity where item_id = new.item_id;
             return null; -- use of trigger return is valid on before trigger only
         end;
     $$;
 
 create trigger updateInsertMSTR
-    after insert or update
+    after insert
     on item_tr
     for each row
     execute function processEdit();
 
-select * from item_tr order by item_id;
 select * from item_mstr order by item_id;
 
-insert into item_tr values (10001, 10);
-insert into item_tr values (10002, 9);
-insert into item_tr values (10003, 8);
+insert into item_tr values (10001, 6);
+insert into item_tr values (10002, 5);
+insert into item_tr values (10003, 2);
 
-update item_tr 
-set quantity = quantity - 5
-where item_id = 10001;
-
-select * from item_tr order by item_id;
 select * from item_mstr order by item_id;
 
-
-drop table item_mstr;
 drop table item_tr;
+drop table item_mstr;
 
+--- 10 , 4 = 6
 
